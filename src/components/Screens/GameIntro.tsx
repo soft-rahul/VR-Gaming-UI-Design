cd7205452ac33497c9cbb1f3c6b4d70eb61e5cbd
Rahul keshri <rkeshri2021@gmail.com>
2024-05-13 00:45:35 +0530

    Intro UI

--- src/components/Screens/GameIntro.tsx
+++ src/components/Screens/GameIntro.tsx
@@ -5,13 +5,44 @@ import GameLogo from "../../assets/game.png";
 import ButtonSkin from "../BTN/Button";
 
 const GameIntro = () => {
+  const playHandler = () => {
+    console.log("How to Play");
+  };
+  const startGameHandler = () => {
+    console.log("Start Game");
+  };
+
   return (
     <Wrapper>
       <div className="intro-container">
         <WrapperText>
-          <div>
-            <ButtonSkin label="Start" onChange={() => console.log("start")} />
-          </div>
+          <section className="intro-content-wrapper">
+            <div className="intro-content">
+              <h1>LASERTAG</h1>
+              <p>
+                Exhilarating and action-packed gaming experience that combines
+                strategic thinking, quick reflexes, and a quest for treasure.
+                Players find themselves in a dynamic and challenging environment
+                where they must navigate through a series of laser-filled
+                obstacles, complete various tasks, and strive to uncover the
+                ultimate treasure.
+              </p>
+              <div>
+                <div className="label-box">
+                  <span className="label-light">Duration</span>
+                  <span className="label-bold"> 9 to 15 mins</span>
+                </div>
+                <div className="label-box">
+                  <span className="label-light">Maximum Player</span>
+                  <span className="label-bold"> 12 Players</span>
+                </div>
+              </div>
+            </div>
+            <div className="intro-buttons">
+              <ButtonSkin label="How To Play" onChange={playHandler} />
+              <ButtonSkin label="Start Game" onChange={startGameHandler} />
+            </div>
+          </section>
         </WrapperText>
         <div className="intro-game-pic">
           <img src={GameLogo} alt="" />
